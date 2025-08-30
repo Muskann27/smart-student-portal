736def53c4cc7b8ab8bbf39c472291dee7bc43bd
Muskann27 <165546240+Muskann27@users.noreply.github.com>
2025-08-30 15:49:54 +0530

    Added Student model and admin integration Added .gitignore to clean repo

--- portal/home/models.py
+++ portal/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    Name = models.CharField(max_length=100)
+    Roll_no = models.CharField(max_length=20, unique=True)
+    Email = models.EmailField(unique=True)
+    Course = models.CharField(max_length=50)
+    Year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.Name} ({self.Roll_no})"
+
